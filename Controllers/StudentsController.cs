e291313619f3b703bb70d80b3efd5954949a1229
Andrii Vlasenko <s19144@pjwstk.edu.pl>
2020-03-30 21:40:21 +0200

    Add project files.

---
+++ Controllers/StudentsController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/students")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        [HttpGet]
+        public string GetStudents()
+        {
+            return "Jan,Anna,Kate";
+        }
+    }
+}
