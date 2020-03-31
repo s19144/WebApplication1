22d146d798831e486777a5590f813e68913b3c4f
Andrii Vlasenko <s19144@pjwstk.edu.pl>
2020-03-31 21:57:52 +0200

    commit

--- Controllers/StudentsController.cs
+++ Controllers/StudentsController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+using WebApplication1.Services;
 
 namespace WebApplication1.Controllers
 {
@@ -10,10 +12,39 @@ namespace WebApplication1.Controllers
     [ApiController]
     public class StudentsController : ControllerBase
     {
-        [HttpGet]
-        public string GetStudents()
+        private IDbService dbService;
+
+        public StudentsController(IDbService db)
         {
-            return "Jan,Anna,Kate";
+            this.dbService = db;
+        }
+
+        [HttpGet]
+        public IActionResult GetStudents(string orderby="firstname")
+        {
+
+            return Ok(dbService.GetStudents());
+        }
+        [HttpGet("{id}")]
+        public IActionResult GetStudent(int id)
+        {
+            if (id == 1)
+            {
+                return Ok("Jan");
+            }else if (id == 2)
+            {
+                return Ok("Kate");
+            }
+
+            return NotFound("Student not found");
+        }
+
+        [HttpPost]
+        public IActionResult CreateStudent(Student student )
+        {
+            student.indexnumber = $"s{new Random().Next(1,20000)}";
+
+            return Ok(student);
         }
     }
 }
---
+++ Services/MockDbService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication1.Models;
+
+namespace WebApplication1.Services
+{
+    public class MockDbService : IDbService
+    {
+        private static IEnumerable<Student> students;
+
+        static MockDbService()
+        {
+            students = new List<Student>
+            {
+                new Student {IdStudent=1,FirstName="Jan",LastName="Covalski",indexnumber="s1234"},
+                new Student {IdStudent=2,FirstName="Anna",LastName="Grob",indexnumber="s4321"},
+                new Student {IdStudent=3,FirstName="Gred",LastName="Brini",indexnumber="s5431"},
+            };
+
+        }
+        public IEnumerable<Student> GetStudents()
+        {
+            return students;
+        }
+    }
+}
